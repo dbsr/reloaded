014c5d35602f03c7e969ab299c04732b99540d2c
Daan Mathot <daanmathot@gmail.com>
2013-06-27 01:48:28 +0200

    init

--- reloaded/plugin/generic.py
+++ reloaded/plugin/generic.py
@@ -3,10 +3,25 @@
 
 import os
 import subprocess
+from functools import wraps
 
 
 class ReloadedPluginException(Exception):
+    '''Subclass this to implement a editor specific error handler.'''
     pass
+
+
+def reloader_error_handler(m):
+    @wraps(m)
+    def wrapper(self, *args, **kwargs):
+        try:
+            m(self, *args, **kwargs)
+        except ReloadedPluginException as e:
+            if self.error_handler:
+                self.error_handler(e)
+            else:
+                raise
+    return wrapper
 
 
 class Plugin(object):
@@ -15,6 +30,9 @@ class Plugin(object):
 
     prc = None
 
+    # Should be a function / lambda accepting two arguments (self, e).
+    error_handler = None
+
     # Default reloaded server settings.
     RUN_SERVER_PY = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'server', 'run.py')
     reloaded_host = 'localhost'
@@ -22,6 +40,7 @@ class Plugin(object):
     reloaded_debug = 0
     reloaded_show_status = 1
 
+    @reloader_error_handler
     def start_server(self, restart=False):
         if not self.poll():
             if not restart:
@@ -39,13 +58,18 @@ class Plugin(object):
         except OSError as e:
             raise ReloadedPluginException(e)
 
+    @reloader_error_handler
     def stop_server(self):
         # Make sure the server is (still) running.
         if not self.poll():
             try:
                 self.prc.kill()
             except OSError as e:
-                raise ReloadedPluginException(e)
+                raise self.error_handler(e)
 
     def poll(self):
         return self.prc.poll() if self.prc else True
+
+    @reloader_error_handler
+    def debug(self):
+        raise ReloadedPluginException('lol')
