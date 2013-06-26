7a5f1491288e640c79978cd4ba6b3c1b3aa52671
Daan Mathot <daanmathot@gmail.com>
2013-06-26 23:10:08 +0200

    init

--- reloaded/plugin/generic.py
+++ reloaded/plugin/generic.py
@@ -17,19 +17,22 @@ class Plugin(object):
 
     # Default reloaded server settings.
     RUN_SERVER_PY = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'server', 'run.py')
-    reloaded_server_host = 'localhost'
-    reloaded_server_port = 9000
+    reloaded_host = 'localhost'
+    reloaded_port = 9000
     reloaded_debug = 0
     reloaded_show_status = 1
 
-    def start_server(self):
+    def start_server(self, restart=False):
         if not self.poll():
+            if not restart:
+                return
             self.stop_server()
+
         try:
             self.prc = subprocess.Popen([
                 self.RUN_SERVER_PY,
-                '--host', self.reloaded_server_host,
-                '--port', str(self.reloaded_server_port),
+                '--host', self.reloaded_host,
+                '--port', str(self.reloaded_port),
                 '--debug', str(self.reloaded_debug),
                 '--show-status', str(self.reloaded_show_status)
             ])
